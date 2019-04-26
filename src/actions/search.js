ac7718fecdda0f87545b2347196a93725aadc6a1
winniefong1021 <winniefong21@gmail.com>
2019-04-25 17:00:59 -0700

    Implement debounce on handleSearch

--- src/actions/search.js
+++ src/actions/search.js
@@ -8,11 +8,12 @@ var handleSearchChange = (q) => {
   // console.log(q)
   //TODO:  Write an asynchronous action to handle a video search!
   // return anon func that calls searchYoutube with q passed in 
-  return (dispatch) => { searchYouTube({key: YOUTUBE_API_KEY, query: q}, (items) => {
-    //console.log('items: ', { dispatch(changeVideo(items[0])) });
-    dispatch(changeVideo(items[0]));
-    dispatch(changeVideoList(items));
-  });}
+  return _.debounce((dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q}, (items) => {
+      dispatch(changeVideo(items[0]));
+      dispatch(changeVideoList(items));
+    });
+  }, 5000);
 };
 
 export default handleSearchChange;
